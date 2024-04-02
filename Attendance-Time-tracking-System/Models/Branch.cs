0c463227211f9be1c2afe901b84837b613c71766
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2024-04-03 00:07:25 +0200

    code first v1

---
+++ Attendance-Time-tracking-System/Models/Branch.cs
@@ -0,0 +1,18 @@
+﻿using Attendance_Time_tracking_System.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Attendance_Time_tracking_System.Models;
+
+public class Branch
+{
+    [Key]
+    public Guid Id { get; set; }
+    
+    [Required]
+
+    [StringLength(100,MinimumLength =5)]
+    public string Name { get; set; }
+
+    [Required]
+    public BranchStatus Status { get; set; }
+}
