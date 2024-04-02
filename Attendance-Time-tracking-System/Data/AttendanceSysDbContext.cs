14831497db4bfa6b369cc9e774e6439f02d475b2
Fatma Atef <fatma.atef.abdelhady@gmail.com>
2024-04-02 07:34:48 +0200

    add classes

--- Attendance-Time-tracking-System/Data/AttendanceSysDbContext.cs
+++ Attendance-Time-tracking-System/Data/AttendanceSysDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Attendance_Time_tracking_System.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Attendance_Time_tracking_System.Data
 {
     public class AttendanceSysDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
     }
    
 }
