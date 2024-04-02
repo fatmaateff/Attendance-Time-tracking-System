3051ada5b60f29d3c072fca4efbae7ef31f27fc8
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2024-04-03 00:07:25 +0200

    code first v1

---
+++ Attendance-Time-tracking-System/Models/Track.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Attendance_Time_tracking_System.Models;
+
+public class Track
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(100 ,MinimumLength =5)]
+    public string Name { get; set; }
+}
