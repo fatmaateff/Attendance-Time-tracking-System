3c10012f5f7e290394aa2b05523d39e82fccf070
ahmed fahmy <fahomahmed@gmail.com>
2024-04-03 23:52:03 +0200

    adding the nav props and FK to my classes

--- Attendance-Time-tracking-System/Models/TrackSchedule.cs
+++ Attendance-Time-tracking-System/Models/TrackSchedule.cs
@@ -1,14 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Attendance_Time_tracking_System.Models
 {
     public class TrackSchedule
     {
+        [ForeignKey("Schedule")]
         public int ScheduleID { get; set; }
+        [ForeignKey("Intake")]
         public int IntakeID { get; set; }
-        [Required]
+        [Required, ForeignKey("Branch")]
         public int BranchID { get; set; }
-        [Required]
+        [Required, ForeignKey("Track")]
         public int TrackID { get; set; }
+
+        //navigation properties
+        public virtual Schedule Schedule { get; set; }
+        public virtual Intake Intake { get; set; }
+        public virtual Branch Branch { get; set; }
+        public virtual Track Track { get; set; }
     }
 }
