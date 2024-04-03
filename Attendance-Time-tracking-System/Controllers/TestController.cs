90df439b4832a9d0faf93a904a1e1bc552d4167f
Ahmed Yousry Helal <ahmedu3helal@gmail.com>
2024-04-03 23:59:55 +0200

    create main layout

---
+++ Attendance-Time-tracking-System/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Attendance_Time_tracking_System.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
