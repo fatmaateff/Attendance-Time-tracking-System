5a954a21b56e045f034826d9eacb5eab60b0d08b
Fatma Atef <fatma.atef.abdelhady@gmail.com>
2024-04-04 13:23:32 +0200

    remove services

--- Attendance-Time-tracking-System/Program.cs
+++ Attendance-Time-tracking-System/Program.cs
@@ -10,16 +10,16 @@ namespace Attendance_Time_tracking_System
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-            builder.Services.AddScoped<IBranchRepository, BranchRepository>();
-            builder.Services.AddScoped<IAttendanceRepository, AttendanceRepository>();
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<ITrackRepository, TrackRepository>();
-            builder.Services.AddScoped<IProgramRepository, ProgramRepository>();
-            builder.Services.AddScoped<IPermissionRepository, PermissionRepository>();
-            builder.Services.AddScoped<IStudentRepository, StudentRepository>();
-            builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
-            builder.Services.AddScoped<ISupervisorRepository, SupervisorRepository>();
+            //builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            //builder.Services.AddScoped<IBranchRepository, BranchRepository>();
+            //builder.Services.AddScoped<IAttendanceRepository, AttendanceRepository>();
+            //builder.Services.AddScoped<IUserRepository, UserRepository>();
+            //builder.Services.AddScoped<ITrackRepository, TrackRepository>();
+            //builder.Services.AddScoped<IProgramRepository, ProgramRepository>();
+            //builder.Services.AddScoped<IPermissionRepository, PermissionRepository>();
+            //builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+            //builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
+            //builder.Services.AddScoped<ISupervisorRepository, SupervisorRepository>();
 
 
 
