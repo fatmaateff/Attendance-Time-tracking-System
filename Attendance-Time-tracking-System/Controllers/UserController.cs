be6820b26dd2159c40b48760121faaddb194b9ed
Ahmed Yousry Helal <ahmedu3helal@gmail.com>
2024-04-05 15:56:57 +0200

    Creating UserController and View user profile

---
+++ Attendance-Time-tracking-System/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Attendance_Time_tracking_System.Data;
+using Attendance_Time_tracking_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Attendance_Time_tracking_System.Controllers
+{
+    public class UserController : Controller
+    {
+        AttendanceSysDbContext context = new AttendanceSysDbContext();
+
+        public IActionResult Profile(int id)
+        {
+            User userModel = context.Users.Include(u => u.Branch).FirstOrDefault(e => e.Id == id);
+
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+    }
+}
