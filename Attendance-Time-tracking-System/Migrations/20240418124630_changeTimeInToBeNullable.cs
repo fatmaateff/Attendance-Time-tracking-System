7045f6568f9cfc86d005b737b259c0e5e705c689
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2024-04-18 17:22:58 +0200

    security complete2

---
+++ Attendance-Time-tracking-System/Migrations/20240418124630_changeTimeInToBeNullable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Attendance_Time_tracking_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeTimeInToBeNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "TimeIn",
+                table: "Attendances",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "TimeIn",
+                table: "Attendances",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "time",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Attendance-Time-tracking-System/ViewModels/UserAttendanceVM.cs
@@ -0,0 +1,16 @@
+﻿namespace Attendance_Time_tracking_System.ViewModels;
+
+public class UserAttendanceVM
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly? TimeIn { get; set; }
+
+    public TimeOnly? TimeOut { get; set; }
+
+    public AttendanceStatus Status { get; set; }
+}
